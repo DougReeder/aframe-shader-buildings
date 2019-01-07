059cfb5a3261bffe5be276e53847cbb2ebfb0787
P. Douglas Reeder <reeder.29@gmail.com>
2019-01-06 23:24:22 -0500

    Shader respects x-, z- and y-proportions, so style can vary by entity

--- aframe-shader-buildings.js
+++ aframe-shader-buildings.js
@@ -81,6 +81,9 @@ AFRAME.registerGeometry('ell', {
 
 AFRAME.registerShader('buildings', {
     schema: {
+        xProportion: {type: 'number', default: 5},
+        zProportion: {type: 'number', default: 5},
+        yProportion: {type: 'number', default: 4},
         wallColor: {type: 'color', default: '#909090'},   // off-white, like concrete
         windowColor: {type: 'color', default: '#181818'},   // dark gray
         sunPosition: {type: 'vec3', default: {x:-1.0, y:1.0, z:-1.0}}
@@ -107,6 +110,9 @@ precision mediump float;
 
 const float PI = 3.1415926535897932384626433832795;
 
+uniform float xProportion;
+uniform float zProportion;
+uniform float yProportion;
 uniform vec3 wallColor;
 uniform vec3 windowColor;
 
@@ -114,13 +120,13 @@ varying vec3 pos;
 varying float sunFactor;
 
 void main() {
-    float xx1 = step(0.0, sin(pos.x * 2.0 * PI / 5.0 - PI / 2.0));
-    float xx2 = step(0.8, sin(pos.z * 2.0 * PI / 5.0 + PI / 2.0));
+    float xx1 = step(0.0, sin(pos.x * 2.0 * PI / xProportion - PI / 2.0));
+    float xx2 = step(0.8, sin(pos.z * 2.0 * PI / zProportion + PI / 2.0));
 
-    float zz1 = step(0.0, sin(pos.z * 2.0 * PI / 5.0 - PI / 2.0));
-    float zz2 = step(0.8, sin(pos.x * 2.0 * PI / 5.0 + PI / 2.0));
+    float zz1 = step(0.0, sin(pos.z * 2.0 * PI / zProportion - PI / 2.0));
+    float zz2 = step(0.8, sin(pos.x * 2.0 * PI / xProportion + PI / 2.0));
 
-    float yy1 = step(0.4, sin(pos.y * 2.0 * PI / 4.0 - 2.3));
+    float yy1 = step(0.4, sin(pos.y * 2.0 * PI / yProportion - 2.0));
 
     vec3 inherentColor = mix(wallColor, windowColor, (xx1 * xx2 + zz1 * zz2) * yy1);
 
@@ -135,6 +141,9 @@ void main() {
         let sunPos = new THREE.Vector3(data.sunPosition.x, data.sunPosition.y, data.sunPosition.z);
         this.material = new THREE.ShaderMaterial({
             uniforms: {
+                xProportion: {value: data.xProportion},
+                zProportion: {value: data.zProportion},
+                yProportion: {value: data.yProportion},
                 wallColor: {value: new THREE.Color(data.wallColor)},
                 windowColor: {value: new THREE.Color(data.windowColor)},
                 sunNormal: {value: sunPos.normalize()}
@@ -148,6 +157,9 @@ void main() {
      * `update` used to update the material. Called on initialization and when data updates.
      */
     update: function (data) {
+        this.material.uniforms.xProportion.value = data.xProportion;
+        this.material.uniforms.zProportion.value = data.zProportion;
+        this.material.uniforms.yProportion.value = data.yProportion;
         this.material.uniforms.wallColor.value.set(data.wallColor);
         this.material.uniforms.windowColor.value.set(data.windowColor);
         let sunPos = new THREE.Vector3(data.sunPosition.x, data.sunPosition.y, data.sunPosition.z);
@@ -193,11 +205,14 @@ AFRAME.registerPrimitive('a-shader-buildings', {
     },
 
     mappings: {
-        'x-proportion': 'geometry.xProportion',
-        'z-proportion': 'geometry.zProportion',
-        'y-proportion': 'geometry.yProportion',
+        'x-proportion-geometry': 'geometry.xProportion',
+        'z-proportion-geometry': 'geometry.zProportion',
+        'y-proportion-geometry': 'geometry.yProportion',
         'buildings': 'geometry.buildings',
 
+        'x-proportion-material': 'material.xProportion',
+        'z-proportion-material': 'material.zProportion',
+        'y-proportion-material': 'material.yProportion',
         'wall-color': 'material.wallColor',
         'window-color': 'material.windowColor',
         'sun-position': 'material.sunPosition',
