52c803baa35d4337b02485992994e2f1116d6a97
P. Douglas Reeder <reeder.29@gmail.com>
2019-01-12 00:05:04 -0500

    Splits source into multiple files & uses webpack to combine them

--- README.md
+++ README.md
@@ -2,7 +2,6 @@ aframe-shader-buildings
 ===
 
 An [A-Frame](https://aframe.io) [WebXR](https://webvr.info/) component for cheaply creating boxy buildings.
-Ell-shaped buildings use only 12 triangles, regardless of the number of windows.
 Allows you to place thousands of buildings in your scene!
 Details are perfectly sharp, no matter how close you get.
 
@@ -10,13 +9,16 @@ Details are perfectly sharp, no matter how close you get.
 
 [live example scene](https://dougreeder.github.io/aframe-shader-buildings/example.html)
 
+Usage
+---
+
 Include using 
 ```html
-<script src="https://unpkg.com/aframe-shader-buildings@^0.5.0/aframe-shader-buildings.js"></script>
+<script src="https://unpkg.com/aframe-shader-buildings@^0.5.0/dist/main.js"></script>
 ```
 
 
-Basic use:
+Declaration of a single two-tiered building:
 ```html
 <a-shader-buildings sun-position="-0.5 1.0 1.0" buildings=
 		'[{"x":5,"z":-995,"xSections":12,"xWingSections":5,"zSections":12,"zWingSections":5,"ySections":30},{"x":0,"z":-1000,"y":120,"xSections":9,"xWingSections":4,"zSections":9,"zWingSections":4,"ySections":30}]'
@@ -28,3 +30,17 @@ to use single quotes around the attribute value, which all modern browsers parse
 
 Normally, the `buildings` attribute is set programatically.
 
+
+
+Internals
+---
+The geometry of all buildings of a single entity is merged, so there's only one draw call.
+
+Ell-shaped buildings use only 12 triangles per tier, regardless of the number of windows.
+
+
+
+
+To Do
+---
+More building shapes: cee, eee and box, for starters.
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-// aframe-shader-buildings.js - boxy buildings using a fast shader for A-Frame WebXR
+// index.js - boxy buildings using a fast shader for A-Frame WebXR
 // Copyright © 2019 by P. Douglas Reeder under the MIT License
+
+import buildingsShader from './buildingsShader';
 
 AFRAME.registerGeometry('ell', {
     schema: {
@@ -78,102 +80,6 @@ AFRAME.registerGeometry('ell', {
     }
 });
 
-
-AFRAME.registerShader('buildings', {
-    schema: {
-        xProportion: {type: 'number', default: 5},
-        zProportion: {type: 'number', default: 5},
-        yProportion: {type: 'number', default: 4},
-        windowWidth: {type: 'number', default: 0.0, min: -1.0, max: 1.0},
-        windowHeight: {type: 'number', default: -0.4, min: -1.0, max: 1.0},
-        wallColor: {type: 'color', default: '#909090'},   // off-white, like concrete
-        windowColor: {type: 'color', default: '#181818'},   // dark gray
-        sunPosition: {type: 'vec3', default: {x:-1.0, y:1.0, z:-1.0}}
-    },
-
-    vertexShader: `
-precision mediump float;
-
-uniform vec3 sunNormal;
-
-varying vec3 pos;
-varying float sunFactor;
-
-void main() {
-  pos = position;
-
-  sunFactor = 0.5 + max(dot(normal, sunNormal), 0.0);
-
-  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-}`,
-
-    fragmentShader: `
-precision mediump float;
-
-const float PI = 3.1415926535897932384626433832795;
-
-uniform float xProportion;
-uniform float zProportion;
-uniform float yProportion;
-uniform float windowWidth;
-uniform float windowHeight;
-uniform vec3 wallColor;
-uniform vec3 windowColor;
-
-varying vec3 pos;
-varying float sunFactor;
-
-void main() {
-    float xx1 = step(windowWidth, sin(pos.x * 2.0 * PI / xProportion - PI / 2.0));
-    float xx2 = step(0.8, sin(pos.z * 2.0 * PI / zProportion + PI / 2.0));
-
-    float zz1 = step(windowWidth, sin(pos.z * 2.0 * PI / zProportion - PI / 2.0));
-    float zz2 = step(0.8, sin(pos.x * 2.0 * PI / xProportion + PI / 2.0));
-
-    float yy1 = step(windowHeight, sin(pos.y * 2.0 * PI / yProportion - 2.0));
-
-    vec3 inherentColor = mix(wallColor, windowColor, (xx1 * xx2 + zz1 * zz2) * yy1);
-
-    gl_FragColor = vec4(inherentColor * sunFactor, 1.0);
-}`,
-
-    /**
-     * `init` used to initialize material. Called once.
-     */
-    init: function (data) {
-        // console.log("wallColor:", new THREE.Color(data.wallColor), "   windowColor:", new THREE.Color(data.windowColor));
-        let sunPos = new THREE.Vector3(data.sunPosition.x, data.sunPosition.y, data.sunPosition.z);
-        this.material = new THREE.ShaderMaterial({
-            uniforms: {
-                xProportion: {value: data.xProportion},
-                zProportion: {value: data.zProportion},
-                yProportion: {value: data.yProportion},
-                windowWidth: {value: -data.windowWidth},
-                windowHeight: {value: -data.windowHeight},
-                wallColor: {value: new THREE.Color(data.wallColor)},
-                windowColor: {value: new THREE.Color(data.windowColor)},
-                sunNormal: {value: sunPos.normalize()}
-            },
-            vertexShader: this.vertexShader,
-            fragmentShader: this.fragmentShader
-        });
-    },
-
-    /**
-     * `update` used to update the material. Called on initialization and when data updates.
-     */
-    update: function (data) {
-        this.material.uniforms.xProportion.value = data.xProportion;
-        this.material.uniforms.zProportion.value = data.zProportion;
-        this.material.uniforms.yProportion.value = data.yProportion;
-        this.material.uniforms.windowWidth.value = -data.windowWidth;
-        this.material.uniforms.windowHeight.value = -data.windowHeight;
-        this.material.uniforms.wallColor.value.set(data.wallColor);
-        this.material.uniforms.windowColor.value.set(data.windowColor);
-        let sunPos = new THREE.Vector3(data.sunPosition.x, data.sunPosition.y, data.sunPosition.z);
-        this.material.uniforms.sunNormal.value = sunPos.normalize();
-    },
-});
 
 
 AFRAME.registerPrimitive('a-shader-buildings', {
